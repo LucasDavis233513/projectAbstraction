d469f83972f8d6e4c2a5398e0e848ef5a119bd0b
LucasDavis233513 <lucasdavis2335@gmail.com>
2023-11-05 18:44:30 -0800

    Fixed some errors, implemented a go-back-n, and initial attempts at a dynamic window and receiver size

--- client.py
+++ client.py
@@ -1,96 +1,118 @@
+import os
 import socket
 import time
 
-WINDOW_SIZE = 10
-
 def checkSum(_data):
     checksum = 0
+    # Convert a list of bytes into a list of integers by list comprehension
+    # Takes two bytes from the data list and converts it to an integer with little-endian order.
+    #   Least significant byte comes firts
     _data = [int.from_bytes(_data[i:i+2], 'little') for i in range(0, len(_data), 2)]
 
+    # Iterate over the new list of int.
     for data16 in _data:
         checksum += data16
 
-        # while there is a
+        # Run if the checksum is larger than 2^16
+        #   right-shifting checksum by 16 and checking if the result is greater than 0
         while ((checksum >> 16) > 0):
-            # Add cary to the sum
+            # Bitwise AND of checksuma and 0xFFFF (65535 or 2^16)
             checksum = (checksum & 0xFFFF) + (checksum >> 16)
 
-    # Ones complement
+    # ~ bitwise NOT operator to flip the bits of checksum\
+    # This with 0xFFFF to keep only the lowest 16 bits of checksum
     checksum = ~checksum & 0xFFFF
 
     return checksum
 
-def printACK(msgReceived):
+def printACK(_ack):
     # if the server did not return a file, exit client program
     # As the connection is likely lost
-    if not msgReceived:
+    if not _ack:
         print("Server did not recieve file, or ACK didn't return")
         print("Connection to the server has been lost. Restart of the")
         print("Client is required")
         exit(1)
     else:
         # Print the reply
-        print("At client: %s"%msgReceived.decode())
+        print("%s\n"%_ack.decode())
 
-def sendFile(_file, _socket):
+def sendFile(_socket,_file):
     try:
-        #Attempt to open file with read bytes option.
+        base = 0
+        seq_num = 0                         # Initialize the sequence numer
+        window = []                         # Create a slinding window buffer to store the packets
+        _socket.settimeout(5.0)             # Set the timeout to 5 seconds
+
+        # Attempt to open file with read bytes option.
         with open(_file, "rb") as fi:
             _socket.send(_file.encode())  # Send the filename to the server
-            seq_num = 0                   # Initialize the sequence numer
-            window_packet = []            # Create a window buffer to store the packets
 
             while True:
                 data = fi.read(1024) #read the file in chunks of 1024 bytes
                 
                 # If data is NULL (all bytes are read) break from the while loop
                 if not data:        
-                    _socket.send("EOF".encode())              # send an EOF message
-                    msgReceived = _socket.recv(1024)          # wait for final ACK message
-                    printACK(msgReceived)                     # print ACK
+                    _socket.send("EOF".encode())      # send an EOF message
+                    ack = _socket.recv(1024)          # wait for final ACK message
+                    printACK(ack)                     # print ACK
                     break
 
+                # Calculate the checksum on the data segment, and append it and the seq_num to the data
+                # to create our data packet: both are encoded using little endian 
+                # Once we have our packet we will append it to the slinding window buffer
                 checksum = checkSum(data)
-                # append the seq_num to the start and checksum to the end of the data bytes and save to a packet
                 packet_to_send =  seq_num.to_bytes(2, 'little') + data + checksum.to_bytes(2, 'little')
-                window_packet.append(packet_to_send)
+                window.append(packet_to_send)
 
-                seq_num += 1                            # increment seq_num
+                # increment seq_num
+                seq_num += 1
 
-                if len(window_packet) == WINDOW_SIZE:
-                    for packet in window_packet:
+                # Once 10 packets have been appended to the buffer we will start to send each to the sever
+                #   Once sent we will wait for an ACK from the server for the bundle of 10 packets
+                if len(window) == WINDOW_SIZE or not data:
+                    for packet in window[base:]:
                         _socket.send(packet)
                         time.sleep(0.01)
 
-                    msgReceived = client.recv(1024)
+                    while True:
+                        try:
+                            ack = _socket.recv(1024)
 
-                    while(True):
-                        if msgReceived.decode() == "CorruptACK":
-                            printACK(msgReceived)
+                            if ack.decode() == "CorruptACK":
+                                print("CorruptACK received, resending data")
+                                for packet in window[base:]:
+                                    _socket.send(packet)
+                                    time.sleep(0.01)
+                                continue
+                            
+                            ack_num = int(ack.decode().split('ACK')[-1])
 
-                            for packet in window_packet:
-                                _socket.send(packet)
-                                time.sleep(0.01)
-                        elif msgReceived.decode().startswith("ACK"):
-                            # Split the message into indicidual acknowledgments
-                            ack_num = msgReceived.decode().split('ACK')[-1:]
-                            ack_num = int(ack_num[0])
+                            if ack_num >= base:
+                                base = ack_num + 1
+                            break
+                        except socket.timeout:
+                            print("timeout occured, resending data")
+                            continue
+                        except (ValueError, IndexError):
+                            print("Invalid ACK received")
+                            continue
 
-                            if ack_num == seq_num:
-                                window_packet.clear()
-                                print("Set of 10 packets recieved")
-                                break
+                    window = window[base:]
+                    base = 0
 
-    except IOError:     # if an error was encountered when reading from the file, we will error out
-        print("You entered an invalid filename\n")
-        print("Please enter a valid name. Eg 'filename.txt'")
+    # if an error was encountered when reading from the file, we will error out
+    except IOError:     
+        print("invalid filename")
+        exit(1)
 
 if __name__ == '__main__':
     file = ""
     host = "192.168.1.25"
     port = 32007
     
-    #create a TCP based client socket
+    # Create a socket using the internet address family IPv4, and specify we wish to
+    #   Use the TCP protocol
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
@@ -100,20 +122,25 @@ if __name__ == '__main__':
         while(True):
             file = input("What is the files name: ")
 
-            # if user inputs and exit cmd, client program will close
+            # interprate the given cmd, if exit close the client, if kill close both server and client
+            #   Otherwise call the sendFile method and pass the socket and filename
             if(file == "exit"):
                 client.close()
                 exit(0)
-            elif(file == "kill"): # if user inputs a kill cmd, client and server program will close            
-                client.send(file.encode())          # send the kill cmd
-                msgReceived = client.recv(1024)     # wait for ACK
-                printACK(msgReceived)               # print the ACK
+            elif(file == "kill"):        
+                client.send(file.encode())      # send the kill cmd
+                ack = client.recv(1024)         # wait for ACK
+                printACK(ack)                   # print the ACK                    
 
                 exit(0)
             else:
-                sendFile(file, client)
+                file_size = os.path.getsize(file)
+                total_packets = (file_size + 1023) // 1024
+                WINDOW_SIZE = min(total_packets, 50)
 
+                client.send(str(WINDOW_SIZE).encode())
+
+                sendFile(client, file)
+    # Return error file if and error occured creating or using the socket
     except socket.error as e:
-        # Return error file if and error occured creating or using the socket
         print(f"Could not connect to the server: {e}")
-
--- server.py
+++ server.py
@@ -1,109 +1,108 @@
 import socket
 import time
 
-WINDOW_SIZE = 10
-
 def checkSum(_data):
     checksum = 0
+    # Convert a list of bytes into a list of integers by list comprehension
+    # Takes two bytes from the data list and converts it to an integer with little-endian order.
+    #   Least significant byte comes firts
     _data = [int.from_bytes(_data[i:i+2], 'little') for i in range(0, len(_data), 2)]
 
+    # Iterate over the new list of int.
     for data16 in _data:
         checksum += data16
 
-        # while there is a carray
+        # Run if the checksum is larger than 2^16
+        #   right-shifting checksum by 16 and checking if the result is greater than 0
         while ((checksum >> 16) > 0):
-            # Add cary to the sum
+            # Bitwise AND of checksuma and 0xFFFF (65535 or 2^16)
             checksum = (checksum & 0xFFFF) + (checksum >> 16)
 
-    # Ones complement
+    # ~ bitwise NOT operator to flip the bits of checksum\
+    # This with 0xFFFF to keep only the lowest 16 bits of checksum
     checksum = ~checksum & 0xFFFF
 
     return checksum
 
-def extractData(_data):    
+def extractData(_data):
     seq_number = int.from_bytes(_data[:2], 'little')  # Extract the first two bytes as the seq_number
     _data = _data[2:]                                 # remove the first two bytes
     checksum = int.from_bytes(_data[-2:], 'little')   # Extract the last two bytes as the checksum
     _data = _data[:-2]                                # remove the last two bytes
+
     return _data, checksum, seq_number
 
-def recieveFile(_socket):
-    data_buffer = []                            # create a data buffer array to store the file bytes
-    window_packets = [None] * WINDOW_SIZE       # Create a window buffer to store the packets
-    base = 0                                    # Initialize the base of the window
+def recieveFile(_socket, file):
+    data_buffer = []                                  # create a data buffer array to store the file bytes
+    excepted_seqNum = 0                               # Initialize the base of the window
+    _socket.settimeout(5.0)                           # Set timeout to 5 seconds
 
     while True:
-        data = _socket.recv(1028)               # Wait for incoming data stream up to 1028 bytes in the reciver window
-                                                # 4 bytes for seq_num and checksum and 1024 for data
+        # Try to get the data packet, if timeout send CorruptACK
+        try:
+            data = _socket.recv(1028)                 # Wait for incoming data stream up to 1028 bytes in the reciver window
+        except socket.timeout:                        # 4 bytes for seq_num and checksum and 1024 for data
+            print("Timeout orccurred, sending CorruptACK")
+            _socket.send("CorruptACK".encode())
+            continue
 
         # Client sends a EOF message to inidicate the end of the data stream
         if data == b'EOF':
             print("File recieved")
 
             # Write all data in the buffer to a file.
-            with open(filename, "wb") as fo:
+            with open(file, "wb") as fo:
                 for data in data_buffer:
                     fo.write(data)
 
             # Clear the data buffer
             data_buffer.clear()
         
+            # Send final ACK
             _socket.send("File recieved".encode())
             time.sleep(0.01)
-
             break
-        elif data:
-            data, receivedCheckSum, recievedSeqNum = extractData(data)
-            serverCheckSum = checkSum(data)                    # Calculate the checksum on recieved data
-
-            # If recievedCheckSum is equal to the serverCheckSum 
-            # add data to the window
-            if receivedCheckSum == serverCheckSum:
-                # Add received data to the window
-                window_packets[recievedSeqNum % WINDOW_SIZE] = data
-
-                # If the base packet has been received, write all packet sto the buffer 
-                # and slide the window
-                while window_packets[base % WINDOW_SIZE] is not None:
-                    data_buffer.append(window_packets[base % WINDOW_SIZE])
-                    window_packets[base % WINDOW_SIZE] = None
-                    base += 1
-
-                if base % 10 == 0:
-                    clientSocket.send(f"ACK{base}".encode())
-                time.sleep(0.01)
-            else:   # otherwise send Corrupt ACK back to client
-                _socket.send("CorruptACK".encode())
-                time.sleep(0.01)
         else:
-            break
+            data, recieved_checksum, recieved_seqNum = extractData(data)  # Extract the seq_num and checksum from packet
+
+            if recieved_checksum == checkSum(data) and recieved_seqNum == excepted_seqNum:
+                data_buffer.append(data)
+                excepted_seqNum += 1
+                _socket.send(f"ACK{excepted_seqNum}".encode())
+            else:
+                _socket.send("CorruptACK".encode())
     
 if __name__ == '__main__':
     host = "192.168.1.25"
     port = 32007
 
-    #A TCP based echo server
+    # Define our socket to use the Internet address family IPv4 and indicate
+    #   That we want to use the TCP protocol
+    # The following will allow us to reuse a local address 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # This line allows the program to reuse the socket address
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     try:
-        #Bind the IP address and the port number
+        # Bind the IP address and the port number &
+        # Listen for incoming connections
+        # Accept an incoming connection and display its information to the screen
         server.bind((host, port))
-
-        #Listen for incoming connections
         server.listen()
-
         (clientSocket, clientAdress) = server.accept()
-
         print("Connection from: " + str(clientAdress))
 
-        #Start accepting client connections
         while(True):
-            filename = clientSocket.recv(1024).decode() # Initial data will be the filename
+            # Set the timeout to none when waiting for a filename
+            clientSocket.settimeout(None)
+
+            WINDOW_SIZE = clientSocket.recv(1024).decode()
+
+            # Accept and initial stream of data from the client
+            #   The server is expecting the filename to be sent first
+            filename = clientSocket.recv(1024).decode()
             
-            #If the filename is set to "kill" the server will close the clients connection and shutdown the server
+            # Interprate the recieved data. If it is a kill cmd shutdown the server
+            #   else call the recieveFile method and pass the socket and filename
             if filename == 'kill':
                 clientSocket.send("ACKed, shutting Down Server".encode())
 
@@ -113,7 +112,7 @@ if __name__ == '__main__':
                 server.close()
                 exit(0) # Ext with status code 0
             else:
-                recieveFile(clientSocket)
+                recieveFile(clientSocket, filename)
 
     except socket.error as e:
         # Return error message if and error occured creating or using the socket
